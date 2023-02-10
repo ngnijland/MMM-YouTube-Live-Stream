1159045f879d3c8ec092f93158146a74f5b4d09f
Niek <ngnijland@gmail.com>
2023-02-10 19:18:54 +0100

    1 check if a youtube channel is streaming (#7)
    * WIP: check if channel is live
    
    * WIP: works, but not headless
    
    * feat: check if channel is streaming based on hardcoded channel id
    
    * feat: make channel configurable
    
    * feat: set timeout to 10 seconds

--- MMM-YouTube-Live-Stream.js
+++ MMM-YouTube-Live-Stream.js
@@ -12,6 +12,38 @@ Module.register('MMM-YouTube-Live-Stream', {
 
   start: function () {
     Log.info(`Starting module: ${this.name}`);
+
+    this.channel = this.config.channel;
+
+    if (typeof this.channel !== 'string' && this.channel !== '') {
+      Log.error(
+        `"${this.channel}" is not a supported value. Please enter a valid channel name`
+      );
+      return;
+    }
+
+    this.sendSocketNotification('GET_CHANNEL_STATUS', this.channel);
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case 'CHANNEL_STATUS': {
+        if (payload.status === 'ERROR') {
+          Log.error(payload.message);
+          return;
+        }
+
+        if (payload.status === 'DONE') {
+          Log.info(`Channel is ${payload.streaming ? '' : 'not '}streaming`);
+        }
+        break;
+      }
+      default: {
+        Log.error(
+          `Socket notivication error: Unknown notification "${notification}" received from node_helper. Please submit an issue in the MMM-YouTube-Live_stream repository.`
+        );
+      }
+    }
   },
 
   getDom: function () {
---
+++ node_helper.js
@@ -0,0 +1,59 @@
+const NodeHelper = require('node_helper');
+const playwright = require('playwright');
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case 'GET_CHANNEL_STATUS': {
+        this.getChannelStatus(payload);
+        break;
+      }
+      default: {
+        this.sendSocketNotification(
+          'ERROR',
+          `Socket notification error: Unknown notification "${notification}" received from module. Please submit an issue in the MMM-YouTube-Live-Stream repository.`
+        );
+      }
+    }
+  },
+
+  getChannelStatus: async function (channel) {
+    this.sendSocketNotification('CHANNEL_STATUS', { status: 'pending' });
+
+    const browser = await playwright.chromium.launch();
+    const context = await browser.newContext({ locale: 'en-US' });
+    const page = await context.newPage();
+
+    await page.goto(`https://www.youtube.com/${channel}/live`);
+
+    try {
+      await page.getByRole('button', { name: 'Reject all' }).click();
+    } catch {
+      this.sendSocketNotification('CHANNEL_STATUS', {
+        status: 'ERROR',
+        message:
+          'Playwright couldn\'t cick "Reject all" in cookie notice. Maybe the YouTube made changes in the page?',
+      });
+    }
+
+    const streamingLocator = page
+      .locator('#description-inner')
+      .getByText(/^Started streaming on/);
+
+    let streaming;
+
+    try {
+      await streamingLocator.waitFor({ timeout: 10000 });
+      streaming = true;
+    } catch {
+      streaming = false;
+    }
+
+    await browser.close();
+
+    this.sendSocketNotification('CHANNEL_STATUS', {
+      status: 'DONE',
+      streaming,
+    });
+  },
+});
